043707a6deb40ca2fe1ce2b74e23953c305db788
Ryan Dial <rdial232@gmail.com>
2022-06-08 09:05:12 -0700

    Added simple UI

--- javascript.js
+++ javascript.js
@@ -17,10 +17,11 @@ function computerPlay(){
 }
 
 function humanPlay(){ //function that will get input from user
-    let choice = prompt("Choose Rock, Paper, Scissors: ")
+    let choice = "Rock";
     return choice;
 
 }
+
 
 
 function playRound(playerSelection, computerSelection){ //function that will determine winner
@@ -61,13 +62,82 @@ function playRound(playerSelection, computerSelection){ //function that will det
 
 function game(){ //play game 5 times
     for (let i=0; i < 5; i++){
-        
-        console.log(playRound(humanPlay(), computerPlay()));
     }
-    
-    
 }
+        
+    
+    
+    
 
 
-game();
+
+//game();
+//console.log(playRound(humanPlay(), computerPlay()));
+
+
+//Creating DOM element for Rock button
+const container = document.querySelector('#container');
+const resultContainer = document.querySelector('#resultContainer');
+
+const rock = document.createElement('button');
+rock.setAttribute('id', 'rock');
+rock.textContent = 'Rock';
+
+container.appendChild(rock); 
+
+//Add event listener to Rock button 
+const btn1 = document.querySelector('#rock');
+btn1.addEventListener('click', () => {
+
+    let result1 = playRound("Rock", computerPlay());
+    const display1 = document.createElement('div');
+    display1.style.border = "thick solid green";
+    display1.textContent = result1;
+
+    resultContainer.appendChild(display1);
+
+});
+
+//Creating DOM element for Paper button
+const paper = document.createElement('button');
+paper.setAttribute('id', 'paper');
+paper.textContent = 'Paper';
+
+container.appendChild(paper); 
+
+//Add event listener to Paper button 
+const btn2 = document.querySelector('#paper');
+btn2.addEventListener('click', () => {
+    let result2 = playRound("Paper", computerPlay());
+    const display2 = document.createElement('div');
+    display2.style.border = "thick solid red";
+    display2.textContent = result2;
+
+    resultContainer.appendChild(display2);
+});
+
+//Creating DOM element for Scissors button
+const scissors = document.createElement('button');
+scissors.setAttribute('id', 'scissors');
+scissors.textContent = 'Scissors';
+
+container.appendChild(scissors); 
+
+//Add event listener to Paper button 
+const btn3 = document.querySelector('#scissors');
+btn3.addEventListener('click', () => {
+    let result3 = playRound("Scissors", computerPlay());
+    const display3 = document.createElement('div');
+    display3.style.border = "thick solid #0390AF";
+    display3.textContent = result3;
+
+    resultContainer.appendChild(display3);
+
+
+});
+
+    
+
+
+
 
